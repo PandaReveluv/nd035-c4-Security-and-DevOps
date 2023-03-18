8d0d4661b4b24ca8400acaf8bfb2726e3f008aa9
PANDAREVELUV-PC\PandaReveluv <dat19071998@gmail.com>
2023-03-18 15:31:01 +0700

    add log when log in failed

--- starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 
 @Service
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -21,6 +23,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         User user = userRepository.findByUsername(username);
         if (user == null) {
+            log.warn("Invalid credentials for username: {}", username);
             return null;
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
